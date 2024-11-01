b886bd62ab4b747d2a50ede9004a7aa1add9f3cd
TristanSoenen <soenentristan@gmail.com>
2024-11-01 18:30:11 +0100

    followed the tutorial until agents

---
+++ StateMachinesVSGoap/Assets/Scripts/UnityLearnTutorialScripts/GAction.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class GAction : MonoBehaviour
+{
+    public string actionName = "Action";
+    public float actionCost = 1.0f;
+    public GameObject target;
+    public GameObject targetTag;
+    public float actionDuration;
+    public WorldState[] preConditions;
+    public WorldState[] afterEffects;
+    public NavMeshAgent agent;
+
+    public Dictionary<string, int> preconditions;
+    public Dictionary<string, int> effects;
+
+    public WorldStates agentBeliefs;
+
+    public bool actionRunning = false;
+
+    public GAction()
+    {
+        preconditions = new Dictionary<string, int>();
+        effects = new Dictionary<string, int>();
+    }
+
+    public void Awake()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+        if (preConditions != null)
+        {
+            foreach(WorldState worldStates in preConditions)
+                preconditions.Add(worldStates.key, worldStates.value);
+        }
+
+        if (afterEffects != null)
+        {
+            foreach (WorldState worldStates in afterEffects)
+                effects.Add(worldStates.key, worldStates.value);
+        }
+    }
+
+    public bool IsAchievalbe()
+    { 
+        return true;
+    }
+
+    public bool IsAchievableGiven(Dictionary<string, int> conditions)
+    {
+        foreach(KeyValuePair<string, int> p in preconditions)
+        {
+            if(!conditions.ContainsKey(p.Key))
+                return false;
+        }
+        return true;
+    }
+
+    public abstract bool PrePerform();
+    public abstract bool PostPerform();
+}
